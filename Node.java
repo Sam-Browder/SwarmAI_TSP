19e3e4f16056d8a7bd2fdc6009bf4284db5a8f05
maurerja <maurerja@rose-hulman.edu>
2016-03-17 19:50:26 -0400

    added node class

---
+++ Node.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+
+public class Node {
+
+	public Node(int i, int numberOfCities) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setNeighbor(Node node, int distance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Node[] getNeighbors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getNeighborID(int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getDistance(int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
